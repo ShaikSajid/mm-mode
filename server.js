a7656df57c3c381d45a0b238b4019a241c41c265
ShaikSajid <sajid.shaik147@gmail.com>
2023-02-17 00:07:56 +0530

    Update server.js
    new images added

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ app.get("/api/categories", (req,res)=>{
 
     const prepareResponse={categories:[
         {title:"Prayer Times Silent", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/p-time-silent.png?alt=media&token=745072f5-a643-4b96-87b9-9945a9cadfde"},
-        {title:"Audio Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/audio-book.png?alt=media&token=03c5d83a-f60b-463d-84ef-9a97d36a67a7"},
-        {title:"All Language Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
-        {title:"99 Names of Allah", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/allah.png?alt=media&token=340256b1-1dbb-4582-ad2e-2bd0e292c230"},
-        {title:"Duas", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/2%20(1).png?alt=media&token=d1bcac61-f5ed-408b-b9e0-a1de07069fd9"},
-        {title:"Tasbih", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
-        {title:"Islamic Gallery", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
-        {title:"Masjid Nearby", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
+        {title:"Audio Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/audio-quran.png?alt=media&token=0c51d220-ddb1-4095-906d-e61c821865ba"},
+        {title:"All Language Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/all-lang-quran.png?alt=media&token=29e3d3a8-3224-474a-8f86-a2be2f204c84"},
+        {title:"99 Names of Allah", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/99-names.png?alt=media&token=5b556ac2-ed4b-4276-b362-03937fe28c59"},
+        {title:"Duas", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/duas.png?alt=media&token=da941575-1879-477f-82cd-03084e309376"},
+        {title:"Tasbih", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/zikar.png?alt=media&token=d59ac6a1-139e-413a-97e1-f88a3668b716"},
+        {title:"Islamic Gallery", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/gallery.png?alt=media&token=2fd5fdd7-9890-4a42-949f-a2f72f6abac7"},
+        {title:"Masjid Nearby", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/nearest-masjid.png?alt=media&token=c15f07c5-4528-4469-b403-2d0ab637d5b3"},
         {title:"Qibla Direction", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
     ]}
 res.json(prepareResponse);
