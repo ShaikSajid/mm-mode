146bd7369ecec6ec49d5a238734c60e0e8951bbb
ShaikSajid <sajid.shaik147@gmail.com>
2023-02-16 23:40:01 +0530

    Update server.js
    duas image added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/api/categories", (req,res)=>{
         {title:"Audio Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/audio-book.png?alt=media&token=03c5d83a-f60b-463d-84ef-9a97d36a67a7"},
         {title:"All Language Quran", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
         {title:"99 Names of Allah", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/allah.png?alt=media&token=340256b1-1dbb-4582-ad2e-2bd0e292c230"},
-        {title:"Duas", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
+        {title:"Duas", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/2%20(1).png?alt=media&token=d1bcac61-f5ed-408b-b9e0-a1de07069fd9"},
         {title:"Tasbih", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
         {title:"Islamic Gallery", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
         {title:"Masjid Nearby", image:"https://firebasestorage.googleapis.com/v0/b/masjid-mode-data.appspot.com/o/quran.png?alt=media&token=19fe3db2-67fb-4142-9107-9c993faf8c05"},
